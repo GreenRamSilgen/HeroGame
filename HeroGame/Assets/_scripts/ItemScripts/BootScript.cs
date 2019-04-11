daaa56779325ee3b7209bd910b41369493c72175
GreenRamSilgen <ps3iswow@gmail.com>
2019-04-10 23:25:54 -0700

    Added currency and fame.
    Can use ResourceTracker.resourceTracker._______ In order to increase, decrease, check fame or currency.
    Need to do this for all items by adding individual item scripts.

---
+++ HeroGame/Assets/_scripts/ItemScripts/BootScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BootScript : MonoBehaviour
+{
+    public Collider2D item;
+    public Collider2D player;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(item.IsTouching(player) && Input.GetKeyDown(KeyCode.G))
+        {
+            ResourceTracker.resourceTracker.IncreaseCurrency(500);
+            ResourceTracker.resourceTracker.IncreaseFans(500);
+        }
+    }
+    
+}
---
+++ HeroGame/Assets/_scripts/ResourceTracker.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceTracker : MonoBehaviour
+{
+    public static ResourceTracker resourceTracker;
+    private double currency;
+    private int fans;
+    public Text currencyTEXT;
+    public Text fansTEXT;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resourceTracker = this;
+        UpdateUI();
+    }
+    
+    public void IncreaseCurrency(double amount)
+    {
+        currency += amount;
+        UpdateUI();
+    }
+    public void DecreaseCurrency(double amount)
+    {
+        currency -= amount;
+        UpdateUI();
+    }
+    public bool hasCurrrencyAmount(double amount)
+    {
+        if (currency >= amount)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void IncreaseFans(int amount)
+    {
+        fans += amount;
+        UpdateUI();
+    }
+    public void DecreaseFans(int amount)
+    {
+        fans -= amount;
+        UpdateUI();
+    }
+    public bool hasFansAmount(int amount)
+    {
+        if (fans >= amount)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+    void UpdateUI()
+    {
+        currencyTEXT.text = "$" + currency.ToString("N2");
+        fansTEXT.text = "Fame:" + fans;
+    }
+}
