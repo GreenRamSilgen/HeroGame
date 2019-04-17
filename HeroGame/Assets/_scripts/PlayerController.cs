434748372eeb68bd4a75cc25da4b5918fb74b056
Mandarker <springnguyen98@gmail.com>
2019-04-17 14:59:19 -0500

    Add content to PlayerController and CameraController scripts

--- HeroGame/Assets/_scripts/PlayerController.cs
+++ HeroGame/Assets/_scripts/PlayerController.cs
@@ -4,23 +4,67 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public float hSpeed;
+    public float vSpeed;
+    public bool grounded = false;
+
+    // Need to standardize
+    public float speed = 0.1F;
+    public float g = -0.01F;
+    public float vSpeedMax = 1;
+
+    private bool oneKey;
+
+    void Start() {
+        hSpeed = 0;
+        vSpeed = 0;
+
+        oneKey = false;
     }
 
-    void FixedUpdate()
-    {
-           if (Input.GetButtonDown("a"))
-        {
-
+    void FixedUpdate() {
+        if (!grounded) {
+            vSpeed += g;
+            if (vSpeed > vSpeedMax)
+                vSpeed = vSpeedMax;
         }
+        else
+            vSpeed = 0;
+
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A)) {
+            if (oneKey) {
+                hSpeed = -hSpeed;
+            }
+            else {
+                hSpeed = 0;
+            }
+
+            oneKey = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) {
+            hSpeed = -speed;
+            oneKey = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) {
+            hSpeed = speed;
+            oneKey = true;
+        }
+        else
+            hSpeed = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector2(transform.position.x + hSpeed, transform.position.y + vSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.gameObject.tag == "Floor") {
+            grounded = true;
+
+            //Fix position
+            int points = collision.contactCount;
+            Debug.Log(points);
+        }
     }
 }
