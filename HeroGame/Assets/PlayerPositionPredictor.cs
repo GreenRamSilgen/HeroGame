9ef4d8fb5f57d922c1da74e9c02dd4197d366b86
Mandarker <springnguyen98@gmail.com>
2019-04-18 17:26:50 -0500

    Add 'predictive' hitbox to determine correct placement of player

---
+++ HeroGame/Assets/PlayerPositionPredictor.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPositionPredictor : MonoBehaviour
+{
+    public PlayerController pc;
+
+    public float hSpeed;
+    public float vSpeed;
+    public bool grounded = false;
+
+    // Need to standardize
+    public float speed = 0.1F;
+    public float g = -0.01F;
+    public float vSpeedMax = 1;
+
+    private bool oneKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hSpeed = 0;
+        vSpeed = 0;
+
+        oneKey = false;
+    }
+
+    void FixedUpdate() {
+        if (!grounded) {
+            vSpeed += g;
+            if (vSpeed > vSpeedMax)
+                vSpeed = vSpeedMax;
+        }
+        else
+            vSpeed = 0;
+
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A)) {
+            if (oneKey) {
+                hSpeed = -hSpeed;
+            }
+            else {
+                hSpeed = 0;
+            }
+
+            oneKey = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) {
+            hSpeed = -speed;
+            oneKey = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) {
+            hSpeed = speed;
+            oneKey = true;
+        }
+        else
+            hSpeed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x + hSpeed, transform.position.y + vSpeed);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        pc.moveOK = false;
+        if (collision.collider.gameObject.tag == "Floor") {
+            int points = collision.contactCount;
+            ContactPoint2D[] contacts = new ContactPoint2D[points];
+            Debug.Log(collision.GetContacts(contacts));
+            Debug.Log(contacts[0].separation);
+
+            //Fix position
+            transform.position = new Vector2(transform.position.x, transform.position.y - (contacts[0].separation * 2));
+
+            grounded = true;
+
+
+        }
+    }
+}
--- HeroGame/Assets/_scripts/PlayerController.cs
+++ HeroGame/Assets/_scripts/PlayerController.cs
@@ -4,67 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float hSpeed;
-    public float vSpeed;
-    public bool grounded = false;
-
-    // Need to standardize
-    public float speed = 0.1F;
-    public float g = -0.01F;
-    public float vSpeedMax = 1;
-
-    private bool oneKey;
-
+    public Transform predict;
+    public bool moveOK;
+    
     void Start() {
-        hSpeed = 0;
-        vSpeed = 0;
-
-        oneKey = false;
+        moveOK = false;
     }
 
-    void FixedUpdate() {
-        if (!grounded) {
-            vSpeed += g;
-            if (vSpeed > vSpeedMax)
-                vSpeed = vSpeedMax;
-        }
-        else
-            vSpeed = 0;
-
-        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A)) {
-            if (oneKey) {
-                hSpeed = -hSpeed;
-            }
-            else {
-                hSpeed = 0;
-            }
-
-            oneKey = false;
-        }
-        else if (Input.GetKeyDown(KeyCode.A)) {
-            hSpeed = -speed;
-            oneKey = true;
-        }
-        else if (Input.GetKeyDown(KeyCode.D)) {
-            hSpeed = speed;
-            oneKey = true;
-        }
-        else
-            hSpeed = 0;
-    }
+    
 
     void Update()
     {
-        transform.position = new Vector2(transform.position.x + hSpeed, transform.position.y + vSpeed);
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
-        if (collision.collider.gameObject.tag == "Floor") {
-            grounded = true;
 
-            //Fix position
-            int points = collision.contactCount;
-            Debug.Log(points);
+        // Here do the calculations? do it separately
+        if (moveOK) {
+            transform.position = predict.position;
+            moveOK = false;
         }
     }
+
 }
